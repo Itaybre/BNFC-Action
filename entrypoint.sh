be8b52cee63c0446e09bdd6b76b0b84d47f1f011
Itay <itay@itaysoft.com>
2020-09-08 22:13:43 -0300

    Add path option

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,13 @@ echo "List files"
 
 cd /github/workspace
 
+cd $1
+
 ls
 
-echo "Building $1"
+echo "Building $2"
 
-bnfc -m $1
+bnfc -m $2
 if [ "$?" -ne 0 ] ; then
   echo "Error building"
   exit 1
@@ -20,37 +22,37 @@ if [ "$?" -ne 0 ] ; then
   exit 1
 fi
 
-./Test$1 /pruebas/1.cc
+./Test$2 /pruebas/1.cc
 if [ "$?" -ne 0 ] ; then
   echo "Error on test 1"
   exit 1
 fi
 
-./Test$1 /pruebas/2.cc
+./Test$2 /pruebas/2.cc
 if [ "$?" -ne 0 ] ; then
   echo "Error on test 2"
   exit 1
 fi
 
-./Test$1 /pruebas/3.cc
+./Test$2 /pruebas/3.cc
 if [ "$?" -ne 0 ] ; then
   echo "Error on test 3"
   exit 1
 fi
 
-./Test$1 /pruebas/4.cc
+./Test$2 /pruebas/4.cc
 if [ "$?" -ne 0 ] ; then
   echo "Error on test 4"
   exit 1
 fi
 
-./Test$1 /pruebas/5.cc
+./Test$2 /pruebas/5.cc
 if [ "$?" -ne 0 ] ; then
   echo "Error on test 5"
   exit 1
 fi
 
-./Test$1 /pruebas/6.cc
+./Test$2 /pruebas/6.cc
 if [ "$?" -ne 0 ] ; then
   echo "Error on test 6"
   exit 1
