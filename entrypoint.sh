70a736894529360845ff622cc41520301d324ffd
Itay <itay@itaysoft.com>
2020-09-08 22:25:13 -0300

    Use variables and loops

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,38 +22,13 @@ if [ "$?" -ne 0 ] ; then
   exit 1
 fi
 
-./Test$2 /pruebas/1.cc
-if [ "$?" -ne 0 ] ; then
-  echo "Error on test 1"
-  exit 1
-fi
+builtfile=Test$(echo "${2%%.*}")
 
-./Test$2 /pruebas/2.cc
-if [ "$?" -ne 0 ] ; then
-  echo "Error on test 2"
-  exit 1
-fi
-
-./Test$2 /pruebas/3.cc
-if [ "$?" -ne 0 ] ; then
-  echo "Error on test 3"
-  exit 1
-fi
-
-./Test$2 /pruebas/4.cc
-if [ "$?" -ne 0 ] ; then
-  echo "Error on test 4"
-  exit 1
-fi
-
-./Test$2 /pruebas/5.cc
-if [ "$?" -ne 0 ] ; then
-  echo "Error on test 5"
-  exit 1
-fi
-
-./Test$2 /pruebas/6.cc
-if [ "$?" -ne 0 ] ; then
-  echo "Error on test 6"
-  exit 1
-fi
+for i in 1 2 3 4 5 6
+do
+    ./$builtfile /pruebas/$i.cc
+    if [ "$?" -ne 0 ] ; then
+        echo "Error on test $i"
+        exit 1
+    fi
+done
